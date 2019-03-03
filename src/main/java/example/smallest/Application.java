8fc6738973da104fab0b9f98c390696269576353
Vinoth <vinoth186@gmail.com>
2019-03-03 19:07:38 +0530

    Update Application.java

--- src/main/java/example/smallest/Application.java
+++ src/main/java/example/smallest/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @ComponentScan
 @EnableAutoConfiguration
