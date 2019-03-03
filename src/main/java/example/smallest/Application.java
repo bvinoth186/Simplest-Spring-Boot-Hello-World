b5c94aab2acc86295a648dc21c72faef1d142458
Vinoth <vinoth186@gmail.com>
2019-03-03 18:55:40 +0530

    Update Application.java

--- src/main/java/example/smallest/Application.java
+++ src/main/java/example/smallest/Application.java
@@ -8,6 +8,11 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 @EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
+    
+    @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
     public static void main(String[] args) {
     	 SpringApplication.run(Application.class, args);
